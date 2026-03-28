970adc38d699913926a95f43ab525ef1b132a266
sunaina-cpu <sunaina@groundai.co>
2026-03-28 21:26:19 +0530

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+  return (
+    <iframe
+      src="https://linkedin-analyst-hjlj2d8y5-linkedin-5323s-projects.vercel.app"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  )
+}
